18db2dcaaaabe65fd6001c010190edba0bcdae50
Andrew Kirov <AndrewKirov@me.com>
2023-09-12 22:49:49 +0500

    minor fix permission

--- yatube_api/api/permission.py
+++ yatube_api/api/permission.py
@@ -4,6 +4,6 @@ from rest_framework import permissions
 class IsPostAuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return super().has_object_permission(request, view, obj)
-        return obj.author == request.user
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+                )
